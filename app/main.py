934b3bc9732a469a53f9ffac312fd0ea2616f758
Ales Raszka <araszka@redhat.com>
2024-12-06 11:42:08 +0100

    new change

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world() -> str:
     """Return a friendly HTTP greeting."""
-    return "<p>Hello, World!</p>"
+    return "<p>Hello from PAC!!</p>"
 
 
 if __name__ == "__main__":
